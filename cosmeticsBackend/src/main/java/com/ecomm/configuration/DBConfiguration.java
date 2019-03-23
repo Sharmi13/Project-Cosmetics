e038d5a3544c53825b7de12412c3aa3e81fef8da
LENOVO <LENOVO@192.168.1.4>
2019-03-23 12:14:59 +0530

    Completed upto Category  in cosmeticsBackend

---
+++ cosmeticsBackend/src/main/java/com/ecomm/configuration/DBConfiguration.java
@@ -0,0 +1,51 @@
+package com.ecomm.configuration;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import com.ecomm.model.Category;
+@Configuration
+@EnableTransactionManagement
+public class DBConfiguration {
+	   public DBConfiguration(){
+		   System.out.println("DBConfiguration class is scanned and bean is created");
+	   }
+
+		@Bean
+	   public DataSource dataSource(){
+		   BasicDataSource dataSource=new BasicDataSource();
+		   dataSource.setDriverClassName("org.h2.Driver");
+		    dataSource.setUrl("jdbc:h2:tcp://localhost/~/cosmetics");
+		    dataSource.setUsername("sa");
+		    dataSource.setPassword("sa");
+		    System.out.println("dataSource bean is created");
+		   return dataSource;
+	   }
+		 @Bean
+		   public SessionFactory sessionFactory(){
+			   LocalSessionFactoryBuilder lsf=new LocalSessionFactoryBuilder(dataSource());
+			   
+			   Properties hibernateProperties=new Properties();
+				hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+				hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+				hibernateProperties.setProperty("hibernate.show_sql", "true");
+				lsf.addProperties(hibernateProperties);
+				lsf.scanPackages("com.ecomm.model");
+				System.out.println("sessionFactory bean is created");
+			   return lsf.buildSessionFactory();
+			   }
+
+		   @Bean
+		   public HibernateTransactionManager hibTransManagement(){
+			   System.out.println("transactionManager bean is created..");
+		   	return new HibernateTransactionManager(sessionFactory());
+		   }
+
+}
---
+++ cosmeticsBackend/src/main/java/com/ecomm/dao/CategoryDAOImpl.java
@@ -0,0 +1,81 @@
+package com.ecomm.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ecomm.model.Category;
+
+@Repository("categoryDAO")
+@Transactional
+public class CategoryDAOImpl implements CategoryDAO
+{
+
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	public boolean addCategory(Category category) 
+	{
+		try
+		{
+			sessionFactory.getCurrentSession().saveOrUpdate(category);
+			return true;
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+	}
+
+	
+	public boolean deleteCategory(Category category) 
+	{
+		try
+		{
+			sessionFactory.getCurrentSession().delete(category);
+			return true;
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+	}
+
+	
+	public boolean updateCategory(Category category) 
+	{
+		try
+		{
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+	}
+
+	
+	public List<Category> listCategories() 
+	{
+		Session session=sessionFactory.openSession();
+		Query query=session.createQuery("from Category");
+		List<Category> listCategories=query.list();
+		session.close();
+		return listCategories;
+	}
+
+	
+	public Category getCategory(int categoryId) 
+	{
+		Session session=sessionFactory.openSession();
+		Category category=(Category)session.get(Category.class,categoryId);
+		session.close();
+		return category;
+	}	
+}
